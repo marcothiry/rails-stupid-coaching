896d0d14a258868ac5d5cf7925fbd5165efe6471
Marc Thiry <marc.thiry@edhec.com>
2017-11-20 11:34:05 +0100

    ask and answer

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,36 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @query = params[:query]
+
+    @answer = coach_answer(@query)
+
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    if your_message == "I AM GOING TO WORK RIGHT NOW!"
+      return ""
+    elsif your_message == your_message.upcase
+      return "I can feel your motivation! #{coach_answer(your_message)}"
+    else
+      return coach_answer(your_message)
+    end
+  end
+
+
+
+
+end
